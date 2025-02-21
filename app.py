259b808390099042c37005ad6e8f61a4e96ebd09
Arman Tiwari <tiwariar279@gmail.com>
2025-02-21 15:15:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,13 +9,7 @@ app = Flask(__name__, static_folder=os.path.join(os.getcwd(), 'frontend', 'dist'
 CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
 
 
-@app.route('/')
-def serve_react():
-    return send_from_directory(app.static_folder, 'index.html')
 
-@app.route('/<path:path>')
-def serve_static_files(path):
-    return send_from_directory(app.static_folder, path)
 
 
 if __name__ == '__main__':
